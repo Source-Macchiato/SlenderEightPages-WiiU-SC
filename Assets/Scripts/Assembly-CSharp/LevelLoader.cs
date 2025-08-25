0373771b85527a5fe78b1f979132787d5b058f98
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-08-25 15:24:00 +0200

    Added loading system

---
+++ Assets/Scripts/Assembly-CSharp/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Start()
+    {
+        StartCoroutine(LoadAsynchronously("Slender"));
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        AsyncOperation operationLoadLevel = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operationLoadLevel.isDone)
+        {
+            float progress = Mathf.Clamp01(operationLoadLevel.progress / 0.9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
