8b450444b22d6e99a7ee944fe7705148eb3aaaed
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-08-28 16:36:42 +0200

    Added assign audio system

---
+++ Assets/Scripts/Assembly-CSharp/Audio/AssignAudio.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using WiiU = UnityEngine.WiiU;
+
+[RequireComponent(typeof(AudioSource))]
+public class AssignAudio : MonoBehaviour
+{
+    [SerializeField] private bool tv = false;
+    [SerializeField] private bool gamepad = false;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (tv && gamepad)
+        {
+            WiiU.AudioSourceOutput.Assign(audioSource, WiiU.AudioOutput.TV | WiiU.AudioOutput.GamePad);
+        }
+        else if (tv)
+        {
+            WiiU.AudioSourceOutput.Assign(audioSource, WiiU.AudioOutput.TV);
+        }
+        else if (gamepad)
+        {
+            WiiU.AudioSourceOutput.Assign(audioSource, WiiU.AudioOutput.GamePad);
+        }
+        else
+        {
+            WiiU.AudioSourceOutput.Assign(audioSource, WiiU.AudioOutput.TV | WiiU.AudioOutput.GamePad);
+        }
+    }
+}
