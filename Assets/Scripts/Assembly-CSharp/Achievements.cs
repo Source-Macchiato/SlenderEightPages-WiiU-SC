e70219e693d024bd7a0ec84147867d41c3186de3
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-08-28 16:23:20 +0200

    Made list of achievements

--- Assets/Scripts/Assembly-CSharp/Achievements.cs
+++ Assets/Scripts/Assembly-CSharp/Achievements.cs
@@ -5,6 +5,15 @@ public class Achievements
 {
 	public enum achievements
 	{
-		
+		FOLLOWS,
+		CANT_RUN,
+		TREES,
+		LEAVE_ME_ALONE,
+		ALWAYS_WATCHES,
+		HELP_ME,
+		DONT_LOOK,
+		NO_NO_NO,
+		SLENDER,
+		THE_EIGHT_PAGES
     }
 }
