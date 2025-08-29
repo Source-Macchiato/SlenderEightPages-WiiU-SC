e739f76ef0e088fa2ae0d54fa1fa822c9a03c3a1
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-08-29 11:14:33 +0200

    Added Source Macchiato intro

---
+++ Assets/Scripts/Assembly-CSharp/UI/RenderMovie.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RenderMovie : MonoBehaviour
+{
+    public MovieTexture movTexture;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        StartCoroutine(Sequence());
+    }
+
+    private IEnumerator Sequence()
+    {
+        movTexture.Play();
+        audioSource.Play();
+
+        yield return new WaitForSeconds(8f);
+
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
