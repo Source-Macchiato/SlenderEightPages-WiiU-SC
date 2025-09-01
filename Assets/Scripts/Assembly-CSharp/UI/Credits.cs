fa2f78ac3a862f66d7d2959b6f00f1dc93a4f4ff
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-09-01 11:21:06 +0200

    Changed delay between texts in credits

--- Assets/Scripts/Assembly-CSharp/UI/Credits.cs
+++ Assets/Scripts/Assembly-CSharp/UI/Credits.cs
@@ -16,79 +16,74 @@ public class Credits : MonoBehaviour
 
 	private IEnumerator CreditsSequence()
 	{
-        titleText.text = string.Empty;
-        descriptionText.text = string.Empty;
-		thanksForPlayingText.text = string.Empty;
-
-		yield return new WaitForSeconds(1f);
-
         titleText.text = "Game Design & Programming";
 		descriptionText.text = "Mark J. Hadley";
+        thanksForPlayingText.text = string.Empty;
 
-		yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(1.5f);
 
 		titleText.text = string.Empty;
 		descriptionText.text = string.Empty;
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(0.5f);
 
 		titleText.text = "Music & Sound";
 		descriptionText.text = "Mark J. Hadley";
 
-		yield return new WaitForSeconds(2.5f);
+		yield return new WaitForSeconds(1.5f);
 
         titleText.text = string.Empty;
         descriptionText.text = string.Empty;
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(0.5f);
 
 		titleText.text = "Models";
 		descriptionText.text = "Pau Cano";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(2f);
 
 		descriptionText.text = "Universal Image";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(2f);
 
 		descriptionText.text = "VIS Games";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(2f);
 
 		descriptionText.text = "Profi Developers";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(2f);
 
 		descriptionText.text = "Unity Technology";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(1.5f);
 
         titleText.text = string.Empty;
         descriptionText.text = string.Empty;
 
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(0.5f);
 
         titleText.text = "Port by Source Macchiato";
         descriptionText.text = "Alexis L.";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(2f);
 
 		descriptionText.text = "Alyx Lihoreau";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(1.5f);
 
         titleText.text = string.Empty;
         descriptionText.text = string.Empty;
 
-		yield return new WaitForSeconds(2.5f);
+		yield return new WaitForSeconds(0.5f);
 
 		thanksForPlayingText.text = "Thanks for playing";
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(1.5f);
 
 		thanksForPlayingText.text = string.Empty;
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(0.3f);
 
         SceneManager.LoadScene("MainMenu");
     }
