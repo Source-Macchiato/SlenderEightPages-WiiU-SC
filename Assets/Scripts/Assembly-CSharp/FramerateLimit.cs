53b6051bb18359f8e0ac74f1de812c961943c322
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-09-16 10:14:51 +0200

    limit framerate to 30FPS

---
+++ Assets/Scripts/Assembly-CSharp/FramerateLimit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FramerateLimit : MonoBehaviour
+{
+	[SerializeField] private int framerate = 60;
+
+	void Awake()
+	{
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = framerate;
+	}
+}
